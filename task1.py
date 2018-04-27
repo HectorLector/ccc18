2eab5d5e265278369e1a0da0ce4ea80b5f136701
Peter Sammer <sammer.p@gmail.com>
2018-04-27 14:59:25 +0200

    muh

--- task1.py
+++ task1.py
@@ -1,7 +1,16 @@
 import os
+import sys
 
-input_file = open("bla.txt", "r")
 
-num = input_file.readline()
-print(num)
-# for line in input_file.
+def parse_input():
+    with open("bla.txt", "r") as f:
+        num = f.readline()
+        content = f.readlines()
+
+        print(content)
+    return num
+
+
+if __name__ == "__main__":
+    num = parse_input()
+    print(num)
